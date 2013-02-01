7a20ff5fcf55d59a8cb0b15a8fbf011c166b3a9b
jinesh <jineshpaloor@gmail.com>
2013-02-01 14:31:13 +0530

    created collection

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,10 +9,25 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/getdata')
+@app.route('/getdata', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def todo_json_resp():
+    print 'get data............',request.method
+    if request.method == 'PUT':
+        print 'updating data  :',request.__dict__
+
+    if request.method == 'POST':
+        print 'saving new data  :',request.__dict__
+
     resp = {'description':'complete kodecrm agent', 'status':'incomplete', 'id':2}
     return json.dumps(resp)
+
+@app.route('/getdata/<id>', methods=['GET', 'POST', 'PUT', 'DELETE'] )
+def get_todo_by_id(id):
+    print 'get data by id............',request.method
+
+    resp = {'description':'respond id 1 description', 'status':'incomplete', 'id':1}
+    return json.dumps(resp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/javascript/todo.js
+++ static/javascript/todo.js
@@ -1,29 +1,153 @@
-var TodoItem = Backbone.Model.extend({});
+$(function(){
 
-//define view to show todo - render todoitem to html element
+    var TodoItem = Backbone.Model.extend({
 
-var TodoView = Backbone.View.extend({
-    render: function (){
-        var html = '<h3>' + this.model.get('description') + '</h3>';
-        $(this.el).html(html);
-    }
+        urlRoot: "/getdata",
 
-});
+        defaults: {
+            description: 'My unedited todo...',
+            status: 'incomplete'
+        },
 
-//var todoItem = new TodoItem({description:'I want to complete backbone tutorial today', status: 'incomplete', id:1});
-var todoItem = new TodoItem({});
-todoItem.url = '/getdata';
-todoItem.fetch({
-    success: function() {
-        // fetch successfully completed
-        console.log('success  :',todoItem.toJSON());
-    },
-    error: function() {
-        console.log('Failed to fetch!');
-    }
+        toggleStatus: function(){
+            if(this.get('status') === 'incomplete'){
+                this.set({'status': 'complete'});
+            }else{
+                this.set({'status': 'incomplete'});
+            }
+            this.save();
+        }
+
     });
-//todoItem.save();
 
-var todoView = new TodoView({model:todoItem});
-todoView.render();
-console.log(todoView.el);
+    var TodoList = Backbone.Collection.extend({
+        model: TodoItem
+    });
+
+    //define view to show todo - render todoitem to html element
+    var TodoView = Backbone.View.extend({
+
+        el: $('#todo-list'),
+
+        events: {
+            'change input': 'toggleStatus'
+        },
+
+        initialize: function (){
+            this.render();
+            this.model.on('change', this.render, this);
+            this.model.on('destroy', this.remove, this);
+        },
+
+        template: _.template('<h3 class="<%= status %>" >' +
+            '<input type=checkbox ' +
+            '<% if(status === "complete") print("checked") %>/>' +
+            '<%= description %></h3>'),
+
+        render: function(){
+            var attributes = this.model.toJSON();
+            this.$el.html(this.template(attributes));
+        },
+
+        toggleStatus: function(){
+            this.model.toggleStatus();
+        },
+
+        remove: function(){
+            this.$el.remove();
+        }
+
+    });
+
+    var TodoListView = Backbone.View.extend({
+
+        render: function(){
+            this.collection.forEach(this.addOne, this );
+        },
+
+        addOne: function(todoItem){
+            var todoView = new TodoView({model: todoItem});
+            console.log('todo view  :',todoView);
+            this.$el.append(todoView.render().el);
+        }
+
+    });
+
+
+
+//    var todoItem = new TodoItem({id:5});
+//    var todoView = new TodoView({model:todoItem});
+    var todoList = new TodoList();
+
+    var todos = [
+        {description: 'Pick up milk.', status: 'incomplete'},
+        {description: 'Get a car wash', status: 'incomplete'},
+        {description: 'Learn Backbone', status: 'incomplete'}
+    ];
+    todoList.reset(todos);
+    var todoListView = new TodoListView({collection: todoList});
+    todoListView.render();
+
+
+    //var todoItem = new TodoItem({description:'I want to complete backbone tutorial today', status: 'incomplete', id:1});
+    //var todoItem1 = new TodoItem({id:1});
+    //var doThing = function(){
+    //    alert('change');
+    //};
+    //todoItem.on('change', doThing);
+    //
+    //todoItem.save({'a':'b'}, {silent: true});
+
+
+    //todoItem.fetch({
+    //    success: function() {
+    //        // fetch successfully completed
+    //        console.log('success  :',todoItem.toJSON());
+    //    },
+    //    error: function() {
+    //        console.log('Failed to fetch!');
+    //    }
+    //});
+    //
+    //todoItem1.fetch({
+    //    success: function() {
+    //        // fetch successfully completed
+    //        console.log('fetch id 1:',todoItem1.toJSON());
+    //    },
+    //    error: function() {
+    //        console.log('Failed to fetch id 1!');
+    //    }
+    //});
+    //
+    //todoItem1.save({'aa':'bb'});
+    //todoItem.destroy({success: function(model, response) {
+    //    console.log('destroy...');
+    //}, error:function(model, response){
+    //    console.log('destroy...fail');
+    //
+    //}});
+    //var todoView = new TodoView({model:todoItem});
+    //todoView.render();
+    //console.log(todoView.el);
+
+    //trying save for model : eg from backbone.js site
+
+    //Backbone.sync = function(method, model) {
+    //  alert(method + ": " + JSON.stringify(model));
+    //  model.id = 1;
+    //};
+    //
+    //var book = new Backbone.Model({
+    //  title: "The Rough Riders",
+    //  author: "Theodore Roosevelt"
+    //});
+    //
+    //book.save();
+    //
+    //console.log('before edit :',book);
+    //
+    //book.save({author: "Teddy"});
+    //
+    //console.log('after edit :',book);
+    //
+});
